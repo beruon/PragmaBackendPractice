7f29b3d1b88651233df8eb198ca6f7e441394fb1
simon.bernal <simon.bernal@SIMONBERNAL>
2022-01-31 10:29:33 -0500

    Creacion de Proyecto con Spring Boot. Con Maven se han instalado las dependencias necesarias para conccion requerida a base de datos,  lombok y la basica de web.

---
+++ src/main/java/com/pragma/restfullapi/sbz/RestFullApiBackendPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.restfullapi.sbz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFullApiBackendPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFullApiBackendPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pragma/restfullapi/sbz/RestFullApiBackendPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.restfullapi.sbz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFullApiBackendPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
